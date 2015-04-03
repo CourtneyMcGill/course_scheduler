45b59464d1ffc3862d80b23362bd6ec4572dbbc1
dmiller1529 <dmiller@colgate.edu>
2015-04-02 20:46:29 -0400

    index, show, new, edit finished in controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,47 @@ class CoursesController < ApplicationController
   def index
 	@courses = Course.sorted_by("crn")
   end
+
   def show
 	@course = Course.find(params[:id])
   end
+
+  def new
+        @course = Course.new
+  end
+
+  def create
+        c = Course.new(safe_params)
+        if c.save
+            flash[:notice] = "Course #{s.title} successfully created"
+            redirect_to courses_path
+        else
+            flash[:warning] = "Course couldn't be created"
+            redirect_to new_course_path
+        end
+  end
+
+  def edit
+        @course = Course.find params[:id]
+  end
+
+  def update
+        @course = Course.find params[:id]
+        @course.update(safe_params)
+        flash[:notice] = "#{@course.title} was successfully updated"
+        redirect_to course_path(@course)
+  end
+
+  def destroy
+        @course = Course.find(params[:id])
+        @course.destroy
+        flash[:notice] = "Course #{@course.title} deleted"
+        redirect_to courses_path
+  end
+
+  private
+  def safe_params
+        params.require(:crn, :course_name, :title, :instructor, :days, :starts, :ends, :building_room, :credits).permit(:coreq, :crosslist, :restrictions, :prereq, :notes)
+  end
+
 end
