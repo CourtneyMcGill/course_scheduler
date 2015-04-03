7f6ebd292789aab7f027f66af2a96778a5749ca4
CourtneyMcGill <cmcgill@colgate.edu>
2015-04-03 15:57:44 -0400

    displaying has many and belongs to stuff in show views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403015138) do
+ActiveRecord::Schema.define(version: 20150403192437) do
 
   create_table "courses", force: :cascade do |t|
     t.integer  "crn"
