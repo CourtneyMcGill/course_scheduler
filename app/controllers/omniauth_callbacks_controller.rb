7539ef560d101dce038672b4dcc21c571af37ab1
dmiller1529 <dmiller@colgate.edu>
2015-04-14 15:41:19 -0400

    login in the process, all files created and user has been modified

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    p request.env["omniauth.auth"]
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = "Successful authentication via github"
+      sign_in_and_redirect @user
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url #change this to redirect nowhere
+    end
+  end
+  
+end
