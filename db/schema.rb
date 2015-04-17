22c51e913f7b9e3243243a7a6b8a13d2454f0b6d
CourtneyMcGill <cmcgill@colgate.edu>
2015-04-16 20:25:44 -0400

    nothing here really

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,14 @@ ActiveRecord::Schema.define(version: 20150414191004) do
     t.integer  "starts"
     t.integer  "ends"
     t.string   "building_room"
-    t.decimal  "credits",       precision: 2, scale: 2
+    t.decimal  "credits",       precision: 10, scale: 2
     t.string   "coreq"
     t.string   "crosslist"
     t.text     "restrictions"
     t.string   "prereq"
     t.text     "notes"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.integer  "student_id"
   end
 
