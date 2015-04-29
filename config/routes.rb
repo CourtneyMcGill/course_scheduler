3fa4da1552db9851a93cf61b9ba4d5a11119f1c8
dmiller1529 <dmiller@colgate.edu>
2015-04-29 12:54:07 -0400

    updating

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
   #end
 
     root 'centeral#index'
-<<<<<<< HEAD
-    resources :courses
-    resources :students
-    resources :requirements
-=======
+    	resources :courses
+    	resources :students
+    	resources :requirements
     resources :courses do
 	resources :students
 	resources :requirements
@@ -25,10 +23,8 @@ Rails.application.routes.draw do
     end
     devise_for :users, :controllers => { omniauth_callbacks: "omniauth_callbacks" }	
     get "/auth/:provider/callback" => 'sessions#create'
-    #get "/users/sign_out" => 'sessions#destroy'
-<<<<<<< HEAD
-=======
-  
->>>>>>> 4d8b5e995e5720fda0906671b119aedacf4ac46f
->>>>>>> 1a628c987696ff4e65e68bdbe2949cb1fab49c65
+
+    devise_for :users do
+      get "/users/sign_out" => 'sessions#destroy'
+    end
 end
