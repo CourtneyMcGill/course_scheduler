20822438442834c6091791f281b90c4aed8c9701
dmiller1529 <dmiller@colgate.edu>
2015-04-29 14:14:01 -0400

    logout method in progress

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,27 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  #before_action :authenticate_user!
+  
   def github
     #p request.env["omniauth.auth"]
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = "Successful authentication via github"
-      sign_in_and_redirect @user #, :event => :authentication
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "github"
+      session[:user_id] = @user.id
+      sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.user_attributes"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
-      #flash[:warning] = "Invalid login, try again or create an account on github"
     end
   end
-  
+
+  def logout
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    #destroy any created session info (blow up 'user_id' and 'devise.user_attributes')
+    
+    @user.remove_all
+    devise.user_attributes.remove_all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,9 @@ Rails.application.routes.draw do
   #end
 
     root 'centeral#index'
-<<<<<<< HEAD
     	resources :courses
     	resources :students
     	resources :requirements
-=======
-    resources :courses
-    resources :students
-    resources :requirements
->>>>>>> 621ae2dd8f712aa2448e4dc77d7c0683909ddd5c
     resources :courses do
 	resources :students
 	resources :requirements
@@ -27,14 +21,13 @@ Rails.application.routes.draw do
     resources :requirements do
 	resources :students
     end
+
     devise_for :users, :controllers => { omniauth_callbacks: "omniauth_callbacks" }	
     get "/auth/:provider/callback" => 'sessions#create'
-<<<<<<< HEAD
+    get "/users/sign_out" => "omniauth_callbacks#logout"
 
-    devise_for :users do
-      get "/users/sign_out" => 'sessions#destroy'
-    end
-=======
+    #devise_for :users do
+    #  get "/users/sign_out" => 'sessions#destroy'
+    #end
     #get "/users/sign_out" => 'sessions#destroy'
->>>>>>> 621ae2dd8f712aa2448e4dc77d7c0683909ddd5c
 end
