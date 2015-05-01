c927eb8c691269d0a4387b99d7047b1e531259e6
Tom Wheeler <ttwheelerb@gmail.com>
2015-05-01 16:32:09 -0400

    Working on testing

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "github"
       session[:user_id] = @user.id
       sign_in_and_redirect @user, :event => :authentication
-      redirect to root
+      redirect_to root
     else
       session["devise.user_attributes"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
