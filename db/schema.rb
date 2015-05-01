1caf416997f7812f5d711b6e58634fb4e105e16d
CourtneyMcGill <cmcgill@colgate.edu>
2015-05-01 03:01:14 -0400

    lots of functionality fixes and styling updates... not pushing so I don't mess anything up

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150501011022) do
-=======
-ActiveRecord::Schema.define(version: 20150429191434) do
->>>>>>> d3c2e35baa27787c8aa110cc79e0977c7871327c
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,7 +62,6 @@ ActiveRecord::Schema.define(version: 20150429191434) do
     t.integer "student_id"
   end
 
-<<<<<<< HEAD
   create_table "plans", force: :cascade do |t|
     t.integer  "course_id"
     t.datetime "created_at", null: false
@@ -78,8 +73,6 @@ ActiveRecord::Schema.define(version: 20150429191434) do
     t.integer "student_id"
   end
 
-=======
->>>>>>> d3c2e35baa27787c8aa110cc79e0977c7871327c
   create_table "requirements", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
